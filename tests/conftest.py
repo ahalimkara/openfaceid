85d90dec023e7b430bf93f3c61ad75c25810b2b7
Abdulhalim Kara <ahalimkara@gmail.com>
2023-07-30 14:29:01 +0100

    Use ./cassettes/{file_name} as vcr cassettes directory

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import pathlib
+
+import pytest
+
+
+@pytest.fixture(scope="module")
+def vcr_cassette_dir(request: pytest.FixtureRequest):
+    # Put all cassettes in ./cassettes/{file_name}/{test}.yaml
+    module_path = request.node.path or pathlib.Path(request.module.__file__)
+    file_name = module_path.stem
+
+    return str(module_path.parent.joinpath("cassettes", file_name))
