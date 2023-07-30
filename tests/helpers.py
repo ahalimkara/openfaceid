1559ee8069072424423710f3a59c5d439b4b5678
Abdulhalim Kara <ahalimkara@gmail.com>
2023-07-30 14:44:27 +0100

    TypeGuard is added in v3.10, hence removing

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,9 +1,9 @@
-from typing import Any, List, TypeGuard
+from typing import Any, List
 
 
-def is_str_list(items: List[Any]) -> TypeGuard[List[str]]:
+def is_str_list(items: List[Any]) -> bool:
     return all(isinstance(item, str) for item in items)
 
 
-def is_float_list(items: List[Any]) -> TypeGuard[List[str]]:
+def is_float_list(items: List[Any]) -> bool:
     return all(isinstance(item, float) for item in items)
